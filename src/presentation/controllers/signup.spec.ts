903b15237d1733901b225be35da7be4714fb5901
Karolinegaia <drakarolinegaia@gmail.com>
2023-07-20 10:33:27 -0300

    fix: errors

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -35,18 +35,17 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
   test('Shold return 400 if  no email is provided', () => {
-    const { sut, emailValidatorStub } = makeSut()
-    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'karol',
         password: 'senha',
-        passowordConfirmation: 'senha'
+        passwordConfirmation: 'senha'
       }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
   test('Shold return 400 if  no password is provided', () => {
     const { sut } = makeSut()
@@ -73,5 +72,34 @@ describe('Signup Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+  test('Shold return 400 if an invalid email is provided', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        name: 'any-karol',
+        email: 'any_email@email.com',
+        password: 'any-senha',
+        passowordConfirmation: 'any-senha'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+  })
+  test('Shold call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        name: 'any_karol',
+        email: 'any_teste@teste.com',
+        password: 'any_senha',
+        passwordConfirmation: 'any_senha'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_teste@teste.com')
   })
 })
